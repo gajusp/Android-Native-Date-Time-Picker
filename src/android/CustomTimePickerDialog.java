9ee4b6a6f7c9a2eea614600e3d9e45c9d7a3ed04
gajusp <gaju.sp@gmail.com>
2016-02-11 13:34:19 +0530

    Added Native Date Time Picker with 15 min interval

---
+++ src/android/CustomTimePickerDialog.java
@@ -0,0 +1,65 @@
+package com.plugin.datepicker;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.widget.NumberPicker;
+import android.widget.TimePicker;
+
+public class CustomTimePickerDialog extends TimePickerDialog {
+
+    private final static int TIME_PICKER_INTERVAL = 15;
+    private TimePicker timePicker;
+    private final OnTimeSetListener callback;
+
+    public CustomTimePickerDialog(Context context, OnTimeSetListener callBack,
+            int hourOfDay, int minute, boolean is24HourView) {
+        super(context, TimePickerDialog.THEME_HOLO_LIGHT, callBack, hourOfDay, minute / TIME_PICKER_INTERVAL,
+                is24HourView);
+        this.callback = callBack;
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        if (callback != null && timePicker != null) {
+            timePicker.clearFocus();
+            callback.onTimeSet(timePicker, timePicker.getCurrentHour(),
+                    timePicker.getCurrentMinute() * TIME_PICKER_INTERVAL);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        try {
+            Class<?> classForid = Class.forName("com.android.internal.R$id");
+            Field timePickerField = classForid.getField("timePicker");
+            this.timePicker = (TimePicker) findViewById(timePickerField
+                    .getInt(null));
+            Field field = classForid.getField("minute");
+
+            NumberPicker mMinuteSpinner = (NumberPicker) timePicker
+                    .findViewById(field.getInt(null));
+            mMinuteSpinner.setMinValue(0);
+            mMinuteSpinner.setMaxValue((60 / TIME_PICKER_INTERVAL) - 1);
+            List<String> displayedValues = new ArrayList<String>();
+            for (int i = 0; i < 60; i += TIME_PICKER_INTERVAL) {
+                displayedValues.add(String.format("%02d", i));
+            }
+            mMinuteSpinner.setDisplayedValues(displayedValues
+                    .toArray(new String[0]));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
